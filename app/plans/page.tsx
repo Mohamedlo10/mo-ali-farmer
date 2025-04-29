d19214051186eba42c5e902fe6d18cf510a49992
Mohamedlo10 <logrealish25@gmail.com>
2025-04-29 16:53:12 +0000

    plans bugs fix

---
+++ app/plans/page.tsx
@@ -0,0 +1,14 @@
+"use client";
+import { useState } from "react";
+import { detectSolEtCultures } from "@/app/api/detection/query";
+import type { Sol, Culture } from "@/interface/type";
+
+export default function PlansPage() {
+  
+
+  return (
+  <div>
+    Plans n'est pas encore disponible
+  </div>
+  );
+}
